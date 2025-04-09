1f4192e4b9f79a27096f5723421de2a8a76dbf6f
apo1397 <apoorvsinha1397@gmail.com>
2025-04-09 19:31:43 +0530

    handling vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out', // Explicitly set the output directory
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 }
 
 export default nextConfig
